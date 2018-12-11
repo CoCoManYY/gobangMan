23eaf25e7290290876659f2cf08d4d8242211f96
CoCoManYY <1344483030@qq.com>
2018-12-11 21:12:09 +0800

    提交文件

---
+++ README.md
@@ -0,0 +1,25 @@
+# 高年级研讨
+
+  fork的别人的仓库，感觉太强了！膜一膜！
+  [大佬的仓库地址](https://github.com/lihongxun945/gobang)
+
+# 安装依赖
+
+先执行 `npm install` 安装依赖。然后有如下命令可用：
+
+- `npm test`  运行单元测试
+- `npm run js` 编译JS
+- `npm run less` 编译less
+- `npm run watch` 进入watch模式 自动编译文件
+
+# 运行
+- `npm server` 即可！
+
+# 基本算法
+
+- 极大极小值搜索
+- Alpha Beta剪枝
+- 启发式评估函数
+- Zobrist缓存
+- 迭代加深
+- ...
---
+++ src/store/modules/board.js
@@ -0,0 +1,106 @@
+import {
+  SET_BOARD,
+  SET_STEPS,
+  ADD_CHESSMAN,
+  RESET_BOARD,
+  SET_FIVES
+} from '../mutations.js'
+
+const getBoard = function (){
+  return [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ]
+}
+
+const copy = function (a) {
+  return a.map((r) => r.slice()).slice()
+}
+
+const state = {
+  board: getBoard(),
+  steps: [
+    /*
+     * like this:
+    {
+      position: [7, 7],
+      role: 1
+    }*/
+  ],
+  stepsTail: [
+  ],
+  fives: []
+}
+
+const getters = {
+  board: state => state.board,
+  steps: state => state.steps,
+  stepsTail: state => state.stepsTail,
+  fives: state => state.fives,
+}
+
+const mutations = {
+  [RESET_BOARD] (state) {
+    state.board = getBoard()
+    state.steps = []
+  },
+  [SET_BOARD] (state, board) {
+    state.board = board
+  },
+  [SET_STEPS] (state, steps) {
+    state.steps = steps
+  },
+  [SET_FIVES] (state, fives) {
+    state.fives = fives
+  },
+  [ADD_CHESSMAN] (state, {position, role}) {
+    let newBoard = copy(state.board)
+    newBoard[position[0]][position[1]] = role
+    state.board = newBoard
+    const step = {
+      position: position,
+      role: role
+    }
+    state.steps.push(step)
+    state.stepsTail = [] //
+  },
+}
+
+const actions = {
+  [RESET_BOARD] ({commit}) {
+    commit(RESET_BOARD)
+  },
+  [SET_BOARD] ({commit}, board) {
+    commit(SET_BOARD, board)
+  },
+  [SET_STEPS] ({commit}, steps) {
+    commit(SET_STEPS, steps)
+  },
+  [SET_FIVES] ({commit}, fives) {
+    commit(SET_FIVES, fives)
+  },
+  [ADD_CHESSMAN] ({commit}, c) {
+    commit(ADD_CHESSMAN, c)
+  },
+}
+
+export default {
+  namespace: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
